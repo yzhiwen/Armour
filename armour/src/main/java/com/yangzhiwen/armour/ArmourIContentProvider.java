d107ebdd0bbf0c8ef260b38bb2a736cd5149636b
YangZhiWen <1425353753@qq.com>
2017-08-20 02:40:35 +0800

    pmd

--- armour/src/main/java/com/yangzhiwen/armour/ArmourIContentProvider.java
+++ armour/src/main/java/com/yangzhiwen/armour/ArmourIContentProvider.java
@@ -1,9 +1,6 @@
 package com.yangzhiwen.armour;
 
-import android.net.Uri;
-
 import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 
